9abbb1b94530f797e83c640ff7d3bbb790cebf51
luojinchun <313282038@qq.com>
2020-02-08 17:50:00 +0800

    五点四十九提交

--- src/components/header/hd.vue
+++ src/components/header/hd.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-  <div id="hd">2222</div>
+  <div id="hd">地摊资源网</div>
+  <ul>
+    <li v-text="msg"></li>
+  </ul>
   </div>
 </template>
 
 <script>
   export default {
     name: 'hd',
-    data () {
+    data :function() {
+      var x=1;
+
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: x+1
       }
     }
   }
@@ -19,8 +24,12 @@
 
   #hd{
   width: 100%;
-      background: red;
-      height: 100px;
+      background: #962727;
+      height: 5rem;
       color: aliceblue;
+      text-align: center;
+      line-height: 5rem;
+      color: white;
+      font-size: 1.5rem;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import hd from '@/components/header/hd'
+// import hd from '@/components/header/hd'
 
 
 Vue.use(Router)
