01f18e7cc21f021cc0815535ef2f6a463619699d
luojinchun <313282038@qq.com>
2020-02-08 13:38:31 +0800

    fsat git

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+<hd/>
+  </div>
+</template>
+
+<script>
+  import hd from '@/components/header/hd'
+export default {
+  components: {
+      hd
+    },
+    name: 'App'
+}
+</script>
+
+<style>
+#app{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+width: 100%;
+}
+</style>
---
+++ src/components/header/hd.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <div id="hd">2222</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hd',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+
+  #hd{
+  width: 100%;
+      background: red;
+      height: 100px;
+      color: aliceblue;
+  }
+</style>
